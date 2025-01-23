4bbf8e8705e7abb4f97c30d92f938bac8d204b1d
bhuvanleopard <104294370+bhuvanleopard@users.noreply.github.com>
2025-01-24 01:03:42 +0530

    Update q12.js

--- assignment/FTSDM01S1_2504/understanding function and reusability/q12.js
+++ assignment/FTSDM01S1_2504/understanding function and reusability/q12.js
@@ -1,7 +1,7 @@
 let greetUser = function(){
   let input=prompt();
   if(input!=false || input!=0){
-    return `Hello, ${input}.`
+    return `Hello, ${input}!.`
   }else{
     return `Hello, Guest!.`
   }
