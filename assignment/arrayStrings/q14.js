1dedfe41feaa51208906d72e08ead75fb9a9668e
bhuvanleopard <104294370+bhuvanleopard@users.noreply.github.com>
2025-01-24 22:56:05 +0530

    Update q14.js

--- assignment/arrayStrings/q14.js
+++ assignment/arrayStrings/q14.js
@@ -1,1 +1,10 @@
-kjlk
+
+let count=0;
+for(let i=0; i<score.length; i++){
+  score[i]>90? score[i]=90:
+    score[i]<40? score[i]+=20:
+    null
+  
+  if(score[i]>=50){count++}  
+}
+console.log(score, count)
