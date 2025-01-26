196865ae62f8dac9f60c1ead150c523c91ad98da
bhuvanleopard <104294370+bhuvanleopard@users.noreply.github.com>
2025-01-26 19:24:18 +0530

    Update Problem4_SimpleInterestCalculator.js

--- assignment/FTSDM01S1_2504/understanding function and reusability/Problem4_SimpleInterestCalculator.js
+++ assignment/FTSDM01S1_2504/understanding function and reusability/Problem4_SimpleInterestCalculator.js
@@ -1,1 +1,19 @@
+function SimpleInterest(p,r,t){
+  let interest=(p*r*t)/100
+  let result = isNaN(interest) || (interest)<0 ? "invalid input":
+  `The simple interest is: ${interest.toFixed(1)} `;
+  return result;
+}
 
+console.log(SimpleInterest(1000,5,3))
+console.log(SimpleInterest(1500,7,5))
+console.log(SimpleInterest(0,6,2))
+
+/**
+LOGIC
+line no2: calculate the interest
+line no3: declared a ternary and checked for NaN
+line no4: executes on interest!=NaN
+line no5: outputs the result
+
+**/
