71fe46446636de3df024b6ba5d3edd8d9208a75e
bhuvanleopard <104294370+bhuvanleopard@users.noreply.github.com>
2025-01-26 18:21:03 +0530

    Update Problem3_LargerNumberIdentifier.js

--- assignment/FTSDM01S1_2504/understanding function and reusability/Problem3_LargerNumberIdentifier.js
+++ assignment/FTSDM01S1_2504/understanding function and reusability/Problem3_LargerNumberIdentifier.js
@@ -1,11 +1,22 @@
 function largerNumber(num1, num2){
-  let result =num1>num2 ? `${num1} is larger than ${num2}`:
+  let result = isNaN(num1*num2) ? "invalid input": 
+              num1>num2 ? `${num1} is larger than ${num2}`:
               num1<num2 ? `${num2} is larger than ${num1}`:
-              num1==num2 ?`Both numbers are equal`: 
-              "invalid input";
+              `Both numbers are equal`; 
+              
 
   return result;
 }
+
+
+/**
+LOGIC
+line no.2: declared a variable assigned to ternary operator, checks for valid input
+line no.3: checks if "num1" is larger
+line no.4: checks if "num2" is larger
+line no.5: if both number are equal this line will be returned;
+
+**/
 
 console.log(largerNumber(5,10))
 console.log(largerNumber(7,7))
