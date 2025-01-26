b20e15eade5548a8c7b0ab8aa0c1f4df35efac23
bhuvanleopard <104294370+bhuvanleopard@users.noreply.github.com>
2025-01-26 18:04:22 +0530

    Update Problem3_LargerNumberIdentifier.js

--- assignment/FTSDM01S1_2504/understanding function and reusability/Problem3_LargerNumberIdentifier.js
+++ assignment/FTSDM01S1_2504/understanding function and reusability/Problem3_LargerNumberIdentifier.js
@@ -1,1 +1,12 @@
+function largerNumber(num1, num2){
+  let result =num1>num2 ? `${num1} is larger than ${num2}`:
+              num1<num2 ? `${num2} is larger than ${num1}`:
+              num1==num2 ?`Both numbers are equal`: 
+              "invalid input";
 
+  return result;
+}
+
+console.log(largerNumber(5,10))
+console.log(largerNumber(7,7))
+console.log(largerNumber(-1,-10))
