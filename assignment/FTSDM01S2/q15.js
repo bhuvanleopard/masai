afae93479d8f41f304c120056e42433be6810fb4
bhuvanleopard <104294370+bhuvanleopard@users.noreply.github.com>
2025-01-29 08:44:31 +0530

    Update q15.js

--- assignment/FTSDM01S2/q15.js
+++ assignment/FTSDM01S2/q15.js
@@ -1,1 +1,15 @@
+function array2D(N){
+  let array=[]
+  let count=1;
+  for(let b=0; b<N; b++){
+    let subArray=[]
+    for(let a=0; a<N; a++){
+      subArray.push(count)
+      count++
+    }
+    array.push(subArray)
+  }
+  return array;
+}
 
+console.log(array2D(3))
