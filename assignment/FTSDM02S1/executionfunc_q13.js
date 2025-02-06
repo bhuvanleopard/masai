73f27cfa015ed344374aaf4b0eb1a1b167a7eedd
bhuvanleopard <104294370+bhuvanleopard@users.noreply.github.com>
2025-02-06 14:54:49 +0530

    Create executionfunc_q13.js

---
+++ assignment/FTSDM02S1/executionfunc_q13.js
@@ -0,0 +1,12 @@
+age
+function displayAge(){console.log(age)};
+displayAge();
+var age=99;
+function displayAge(){console.log(age)};
+function changeAge(){
+  age=100
+  console.log(age)
+};
+
+displayAge();
+changeAge();
