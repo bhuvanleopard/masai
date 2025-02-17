898a3254f09292af373bc58ea350665aa061dec6
bhuvanleopard <104294370+bhuvanleopard@users.noreply.github.com>
2025-02-17 10:58:10 +0530

    Update asyc_q13.js

--- assignment/M02S02/asyc_q13.js
+++ assignment/M02S02/asyc_q13.js
@@ -1,13 +1,30 @@
 
 function timer(time,onComplete){
-  this.time=time
+  
   // onComplete.time=time
   
-  return setTimeout(onComplete, time);
+  return setTimeout(()=>onComplete(time), time);
 }
 
-function any(){
-  console.log(`Timer of ${time} ms finished`)
+function any(ele){
+  console.log(`Timer of ${ele} ms finished`)
 }
 
-timer(1000, any)
+timer(1500, any)
+
+
+
+//somehow even following code works!!!
+
+// function timer(time,onComplete){
+//   this.time=time
+//   // onComplete.time=time
+  
+//   return setTimeout(onComplete, time);
+// }
+
+// function any(){
+//   console.log(`Timer of ${time} ms finished`)
+// }
+
+// timer(1000, any)
